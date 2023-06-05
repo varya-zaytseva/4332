0ba2962aa88301aadc415d1f7bbe4dcdfcf2a33e
Varya <varya.zaytseva@icloud.com>
2023-06-05 11:34:08 +0300

    hgttr

---
+++ data.js
@@ -0,0 +1,21 @@
+export const context = {
+    languages: [
+      {
+        name: 'HTML',
+        modern: true,
+      },
+      {
+        name: 'CSS',
+        modern: true,
+      },
+      {
+        name: 'JavaScript',
+        modern: true,
+      },
+      {
+        name: 'COBOL',
+        modern: false,
+      },
+    ],
+  };
+  
